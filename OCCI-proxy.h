efed2806b7eee9f2584b09878a4cb698b024ed28
Pixel <pixel@nobis-crew.org>
2010-09-12 11:51:26 -0700

    Fixing warning under MSVC, and error under mingw32.

--- OCCI-proxy.h
+++ OCCI-proxy.h
@@ -15,14 +15,14 @@ namespace occi_proxy {
     class Environment {
       public:
         enum Mode {
-            DEFAULT = oracle::occi::Environment::Mode::DEFAULT,
-            OBJECT = oracle::occi::Environment::Mode::OBJECT,
-            SHARED = oracle::occi::Environment::Mode::SHARED,
-            NO_USERCALLBACKS = oracle::occi::Environment::Mode::NO_USERCALLBACKS,
-            THREADED_MUTEXED = oracle::occi::Environment::Mode::THREADED_MUTEXED,
-            THREADED_UNMUTEXED = oracle::occi::Environment::Mode::THREADED_UNMUTEXED,
-            EVENTS = oracle::occi::Environment::Mode::EVENTS,
-            USE_LDAP = oracle::occi::Environment::Mode::USE_LDAP,
+            DEFAULT = oracle::occi::Environment::DEFAULT,
+            OBJECT = oracle::occi::Environment::OBJECT,
+            SHARED = oracle::occi::Environment::SHARED,
+            NO_USERCALLBACKS = oracle::occi::Environment::NO_USERCALLBACKS,
+            THREADED_MUTEXED = oracle::occi::Environment::THREADED_MUTEXED,
+            THREADED_UNMUTEXED = oracle::occi::Environment::THREADED_UNMUTEXED,
+            EVENTS = oracle::occi::Environment::EVENTS,
+            USE_LDAP = oracle::occi::Environment::USE_LDAP,
         };
         ~Environment() throw (SQLException);
         static Environment * createEnvironment(Mode mode, void *ctxp = 0, void *(*malocfp)(void *ctxp, size_t size) = 0, void *(*ralocfp)(void *ctxp, void *memptr, size_t newsize) = 0, void (*mfreefp)(void *ctxp, void *memptr) = 0) throw (SQLException);
