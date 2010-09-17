63db326f8e46d75256248235a9a8eba611090e27
Pixel <pixel@nobis-crew.org>
2010-09-17 00:27:01 -0700

    Adding a slightly better test project.

--- OCCI-proxy-test.cpp
+++ OCCI-proxy-test.cpp
@@ -5,11 +5,31 @@ using namespace occi_proxy;
 
 int main(void) {
     Environment * envr;
+    Connection * conn;
+    Statement * stmt;
+    ResultSet * rset;
 
     try {
         envr = Environment::createEnvironment(Environment::THREADED_MUTEXED);
         printf("Got an environment: %p\n", envr);
+        conn = envr->createConnection("OE", "OE");
+        printf("Got a connection  : %p\n", conn);
+        stmt = conn->createStatement("select * from user_objects where object_type = 'TABLE'");
+        printf("Got a statement   : %p\n", stmt);
+        rset = stmt->executeQuery();
+        printf("Got a resultset   : %p\n", rset);
+        int n = rset->next();
+        printf("rset->next() = %i\n", n);
+        if (n) {
+            std::string s = rset->getString(1);
+            printf("s = %s\n", s.c_str());
+        }
+        stmt->closeResultSet(rset);
+        conn->terminateStatement(stmt);
+        envr->terminateConnection(conn);
+        Environment::terminateEnvironment(envr);
     } catch (SQLException e) {
         printf("Got an exception: %s\n", e.what());
+        e.getMessage();
     }
 }
